57d101c2309e9cff8554fffa1f9cde79469f5f6d
hyegyeonglee <wingedant1@naver.com>
2024-07-31 15:25:50 +0900

    충돌 테스트 중1

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,6 +34,7 @@ import { Link } from 'react-router-dom';
 //   { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
 //   { name: 'Contact sales', href: '#', icon: PhoneIcon },
 // ]
+// 테스트 중입니다 테스트 브랜치
 
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
