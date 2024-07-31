c2c98592443c59010704f9168356e94dcd4a2fed
hyegyeonglee <wingedant1@naver.com>
2024-07-31 15:18:56 +0900

    에러 잡기. 클래스명 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -132,7 +132,7 @@ export default function Example() {
                   비포&에프터
                 </Link>
                 <Link
-                  to ="/"
+                  to ="/chatOtherList"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   채팅
