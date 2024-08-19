98c7140c9f980c3ee148bfa6aae077060d1b1432
hyegyeonglee <wingedant1@naver.com>
2024-08-19 21:41:39 +0900

    코드 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,24 +4,12 @@ import { useState } from 'react'
 import {
   Dialog,
   DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Popover,
-  PopoverButton,
   PopoverGroup,
-  PopoverPanel,
 } from '@headlessui/react'
 import {
-  ArrowPathIcon,
   Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 import { Link } from 'react-router-dom';
 // const products = [
 //   { name: '홈', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
