02cd6931d03cd394349b58d87594dd8100ea163c
hyegyeonglee <wingedant1@naver.com>
2024-08-25 19:48:34 +0900

    header 수정 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -58,7 +58,7 @@ const Header = () => {
 
   return (
     <header className="bg-white">
-      <nav aria-label="Global" className="flex items-center justify-between p-6 mx-auto max-w-7xl lg:px-8">
+      <nav aria-label="Global" className="flex items-center justify-between p-6 mx-auto max-w-7xl lg:px-0">
         <div className="flex lg:flex-1">
           <Link to ="/" className="-m-1.5 p-1.5 flex">
             <span className="sr-only">Your Company</span>
@@ -66,7 +66,7 @@ const Header = () => {
             <p className='ml-2 text-3xl font-black'>JUPJUP</p>
           </Link>
         </div>
-        <div className="flex lg:hidden">
+        <div className="flex">
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
@@ -76,16 +76,14 @@ const Header = () => {
             <Bars3Icon aria-hidden="true" className="w-6 h-6" />
           </button>
         </div>
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+        {/* <PopoverGroup className="hidden lg:flex lg:gap-x-12">
           <Link to ="/" className="text-sm font-black leading-6">
             홈
           </Link>
           <Link to ="/jupjup" className="text-sm font-black leading-6">
             나눔하기
           </Link>
-          {/* <Link to ="/beforeAfter" className="text-sm font-black leading-6">
-            비포&에프터
-          </Link> */}
+
           <Link to ="/chatOtherList" className="text-sm font-black leading-6">
             채팅
           </Link>
@@ -100,8 +98,8 @@ const Header = () => {
           </svg>
 
 
-        </PopoverGroup>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        </PopoverGroup> */}
+        {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
         {isLoggedIn ? (
             <Link onClick={handleLogout} className="text-sm font-semibold leading-6">
               Log out <span aria-hidden="true">&rarr;</span>
@@ -111,9 +109,9 @@ const Header = () => {
               Log in <span aria-hidden="true">&rarr;</span>
             </Link>
           )}
-        </div>
+        </div> */}
       </nav>
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
@@ -138,16 +136,11 @@ const Header = () => {
           <div className="flow-root mt-6">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="py-6 space-y-2">
-                <Link to ="/"
-                  className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
-                >
-                  홈
-                </Link>
                 <Link
                   to ="/jupjup"
                   className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
                 >
-                  나눔하기
+                  줍줍
                 </Link>
                 {/* <Link
                   to ="/beforeAfter"
@@ -171,7 +164,7 @@ const Header = () => {
                   to ="/Mypage"
                   className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
                 >
-                  Mypage
+                  마이페이지
                 </Link>
               </div>
               <div className="py-6">
@@ -180,14 +173,14 @@ const Header = () => {
                     onClick={handleLogout}
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
-                    Log out
+                    로그아웃
                   </Link>
                 ) : (
                   <Link
                     to="/signin"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
-                    Log in
+                    로그인
                   </Link>
                 )}
               </div>
