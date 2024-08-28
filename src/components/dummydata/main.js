7383d9520ae808632539b694faab29f0112fa62c
hyegyeonglee <wingedant1@naver.com>
2024-08-28 14:24:48 +0900

    main화면에 있는 데이터 수정

---
+++ src/components/dummydata/main.js
@@ -0,0 +1,20 @@
+export const mainPosts = [
+    {
+      id: 1,
+      title: '당신의 물건, 누군가의 보물. 줍줍에서 나누고 채우세요.',
+      imageUrl:
+        '/main/maude.jpg',
+    },
+    {
+      id: 2,
+      title: '이사 전 대청소, 걱정 끝! 줍줍으로 쉽고 의미 있게 정리하세요.',
+      imageUrl:
+        '/main/hiveboxx.jpg',
+    },
+    {
+      id: 3,
+      title: '버리기엔 아깝고, 팔기엔 귀찮은 물건들. 줍줍에서 새 주인을 만나세요.',
+      imageUrl:
+        '/main/clay.jpg',
+    },
+]
