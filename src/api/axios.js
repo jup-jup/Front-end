d85726bfc414615598750a24812b35d927239ab4
hyegyeonglee <wingedant1@naver.com>
2024-09-01 17:55:59 +0900

    .env 파일 수정해서 올립니다...

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_ROOT_API,
+  baseURL: process.env.REACT_APP_API_URL,
   timeout: 5000,
   withCredentials: true,
   headers: {
