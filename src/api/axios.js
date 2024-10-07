4d0b0f65e2aa76e7c25cb717dd7f908ae1872030
rudwnok <rudwnok123@naver.com>
2024-10-07 23:04:55 +0900

    401 에러 처리 임시

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   timeout: 5000,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
@@ -13,9 +13,9 @@ const instance = axios.create({
 instance.interceptors.request.use(
   (config) => {
     // 요청 전에 수행할 작업
-    const token = localStorage.getItem('accessToken');
+    const token = localStorage.getItem("accessToken");
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
   },
@@ -30,9 +30,31 @@ instance.interceptors.response.use(
     // 응답 데이터를 가공
     return response;
   },
-  (error) => {
-    // 오류 응답을 처리
-    return Promise.reject(error);
+  async function (error) {
+    if (error.response && error.response.status === 401) {
+      // 쿠키의 리프레쉬가 언디파인드가 아닐때만 토큰 재발급
+      // if (getCookie("dtrtk") !== "undefined") {
+      //   const data = await axios.post(`${ROOT_API}/token/refresh`, {
+      //     // refreshToken: getCookie("dtrtk"),
+      //     headers: {
+      //       accept: "*/*",
+      //       "Content-Type": "application/json",
+      //     },
+      //   });
+
+      //   // 헤더에 담긴 토큰 값 변경
+      //   error.config.headers = {
+      //     "Content-Type": "application/json",
+      //     "X-AUTH-TOKEN": `${data.data.accessToken}`,
+      //   };
+
+      //   // 재요청
+      //   const originalResponse = await axios.request(error.config);
+      //   return originalResponse.data;
+      // }
+      // 토큰 재발급 요청
+      return Promise.reject(error);
+    }
   }
 );
 
