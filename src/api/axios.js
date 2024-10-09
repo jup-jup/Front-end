2f5bc76cd52d25cef1e8c24d3db443b5c50de9f0
rudwnok <rudwnok123@naver.com>
2024-10-10 02:01:24 +0900

    프론트 리이슈 처리 - get 이 맞는지 확인할것. - 확인후 response로 다시 작업할것

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import epochConvert from "util/epochConverter";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
@@ -32,26 +34,37 @@ instance.interceptors.response.use(
   },
   async function (error) {
     if (error.response && error.response.status === 401) {
-      // 쿠키의 리프레쉬가 언디파인드가 아닐때만 토큰 재발급
+      // 토큰을 쿠키로 관리 해야 하고, 
+      // 쿠키의 리프레쉬가 언디파인드가 아닐때만 토큰 재발급으로 변경해야함.
+      // 처음 토큰 저장시에는 refreshtoken의 만료기간을 파악해서 만료기간이 되면 제거 시켜야 함.
+      const date = jwtDecode(localStorage.getItem("accessToken")).exp;
+      // console.log("dd", epochConvert(date));
+
       // if (getCookie("dtrtk") !== "undefined") {
-      //   const data = await axios.post(`${ROOT_API}/token/refresh`, {
-      //     // refreshToken: getCookie("dtrtk"),
-      //     headers: {
-      //       accept: "*/*",
-      //       "Content-Type": "application/json",
-      //     },
-      //   });
+      if (epochConvert(date)) {
+        const data = await axios.post(
+          `${process.env.REACT_APP_API_URL}/v1/auth/reissue`,
+          {
+            refreshToken: localStorage.getItem("refreshToken"),
+            headers: {
+              accept: "*/*",
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
-      //   // 헤더에 담긴 토큰 값 변경
-      //   error.config.headers = {
-      //     "Content-Type": "application/json",
-      //     "X-AUTH-TOKEN": `${data.data.accessToken}`,
-      //   };
+        console.log('c', data);
 
-      //   // 재요청
-      //   const originalResponse = await axios.request(error.config);
-      //   return originalResponse.data;
-      // }
+        // 헤더에 담긴 토큰 값 변경
+        error.config.headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${data.data.accessToken}`,
+        };
+
+        // 재요청
+        const originalResponse = await axios.request(error.config);
+        return originalResponse.data;
+      }
       // 토큰 재발급 요청
       return Promise.reject(error);
     }
